47e67b04b1b35122240852f5e957bf00df96c3f1
frazakhtar <87436611+frazakhtar@users.noreply.github.com>
2025-06-30 21:32:00 +0530

    complete form validation

--- netflix-gpt/src/components/Login.js
+++ netflix-gpt/src/components/Login.js
@@ -1,27 +1,64 @@
-import React, { useState } from 'react'
-import Header from './Header'
+import React, { useRef, useState } from "react";
+import Header from "./Header";
+import {checkValidData} from "../utils/validate"
 
 const Login = () => {
-    const [isSignInForm, setIsSignInForm] = useState(true)
-    const toggleSignInForm = ()=>{
-        setIsSignInForm(!isSignInForm)
-    }
+  const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("");
+  const email = useRef(null)
+  const password = useRef(null)
+  const handleButtonClick = () => {
+    const msg = checkValidData(email.current.value,password.current.value);
+    setErrorMessage(msg)
+  }
+
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
   return (
     <>
-    <Header />
-    <div className='absolute'>
-        <img src="https://assets.nflxext.com/ffe/siteui/vlv3/75b0ed49-75ab-4a63-bd45-37bc2c95cb73/web/IN-en-20250623-TRIFECTA-perspective_ae5833b7-6ce5-4e88-853e-014f38c506f1_medium.jpg" alt="background"/>
-    </div>
-    <form className='absolute w-3/12 p-12 bg-black my-36 mx-auto right-0 left-0 rounded-lg bg-opacity-80'>
-        <h1 className='text-white font-bold text-xl my-8'>{isSignInForm ? "Sign In" : "Sign Up"}</h1>
-        <input type = "text" placeholder='Email Address'className='p-4 my-2 w-full bg-gray-700' />
-        {!isSignInForm && <input type = "text" placeholder='Name'className='p-4 my-2 w-full bg-gray-700' />}
-        <input type = "password" placeholder='Password' className='p-4 my-2 w-full bg-gray-700' />
-        <button className='bg-red-700 py-4 my-8 w-full text-white rounded-lg'>{isSignInForm ? "Sign In" : "Sign Up"}</button>
-        <p className='text-white' onClick={toggleSignInForm}>{isSignInForm ? "New to Netflix? Sign Up Now" : "Already Registered? Sign In Now"}</p>
-    </form>
+      <Header />
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/75b0ed49-75ab-4a63-bd45-37bc2c95cb73/web/IN-en-20250623-TRIFECTA-perspective_ae5833b7-6ce5-4e88-853e-014f38c506f1_medium.jpg"
+          alt="background"
+        />
+      </div>
+      <form onSubmit={(e)=>{e.preventDefault()}} className="absolute w-3/12 p-12 bg-black my-36 mx-auto right-0 left-0 rounded-lg bg-opacity-80">
+        <h1 className="text-white font-bold text-xl my-8">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        <input
+          ref = {email}
+          type="text"
+          placeholder="Email Address"
+          className="p-4 my-2 w-full bg-gray-700"
+        />
+        {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="Name"
+            className="p-4 my-2 w-full bg-gray-700"
+          />
+        )}
+        <input
+        ref = {password}
+          type="password"
+          placeholder="Password"
+          className="p-4 my-2 w-full bg-gray-700"
+        />
+        { errorMessage && <p className="text-red-700">{errorMessage}</p> }
+        <button onClick={handleButtonClick} className="bg-red-700 py-4 my-8 w-full text-white rounded-lg">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="text-white cursor-pointer" onClick={toggleSignInForm}>
+          {isSignInForm
+            ? "New to Netflix? Sign Up Now"
+            : "Already Registered? Sign In Now"}
+        </p>
+      </form>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
---
+++ netflix-gpt/src/utils/validate.js
@@ -0,0 +1,9 @@
+export const checkValidData = (email,password) =>{
+    const isValidEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(email);
+    const isValidPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/g.test(password);
+
+    if(!isValidEmail) return "Email is not valid!!";
+    if(!isValidPassword) return "Password is not valid !!";
+
+    return null;
+};
